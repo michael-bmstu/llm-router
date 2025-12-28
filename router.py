530893bd12562c82aeac601b14e7965cb0fb1ab1
Mihail Solovev <solovei.0807@gmail.com>
2025-12-28 16:00:01 +0300

    feat: add router func

---
+++ router.py
@@ -0,0 +1,34 @@
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+PROMPT = """\
+You work on the admissions committee for a Master's program in Artificial Intelligence, \
+and students write to you with questions about the program, internships, and entrance exams.
+
+Classify the question into one of the following categories: document submission, entrance examinations, curriculum and courses, internships, other.
+Answer format: category name\
+"""
+
+prompt_template = ChatPromptTemplate([
+    ("system", PROMPT),
+    ("user", "{question}")
+])
+
+llm = init_chat_model(
+    model="mistral-medium-latest",
+    api_key=os.getenv("MISTRAL_KEY"),
+    temperature=0)
+
+chain = prompt_template | llm | StrOutputParser()
+
+# question = input("Enter your question: ")
+question = "а на какие стажировки я смогу попасть?"
+ans = chain.invoke({"question": question})
+print(ans)
